78ecb7240036b8a0cb47e8fb645283d4fafb421a
op-gp <nisidhusilva4@gmail.com>
2026-07-05 22:01:31 +0530

    Commented out incomplete endpoints in adminRoutes.ts

--- backend/src/routes/adminRoutes.ts
+++ backend/src/routes/adminRoutes.ts
@@ -7,19 +7,19 @@ const router = express.Router();
 router.get("/users", fetchUsers)
 
 // POST request for creating a category. api/admin/categories/create
-router.post("/categories/create")
+// router.post("/categories/create")
 
 // POST request for creating a quiz. api/admin/quizzes/create
-router.post("/quizzes/create")
+// router.post("/quizzes/create")
 
 // POST request for adding a question to a specific quiz. api/admin/quizzes/<quizId>/questions
-router.post("/quizzes/:quizId/questions")
+// router.post("/quizzes/:quizId/questions")
 
 // PUT request for updating a question of a quiz. api/admin/questions/<questionId>
-router.put("/questions/:questionId")
+// router.put("/questions/:questionId")
 
 // DELETE request for deleting a question of said question id. api/admin/questions/<questionId>
-router.delete("/questions/:questionId")
+// router.delete("/questions/:questionId")
 
 // DELETE request for deleting user of said id. api/admin/users/<id>
 router.delete("/users/:id", deleteUser);
